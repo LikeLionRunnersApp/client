d113171041498a83910db0d5c14dc958b83330d9
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-07-23 16:07:00 +0900

    Fix: Change alertIcon

--- src/stories/UI/Flag.stories.tsx
+++ src/stories/UI/Flag.stories.tsx
@@ -1,13 +1,13 @@
-import Flag from "@components/Common/UI/Flag";
+import Flag from '@components/Common/UI/Flag'
 
 export default {
-  title: "Component/UI/Flag",
+  title: 'Component/UI/Flag',
   component: Flag,
   argsType: {
-    type: { control: "radio", option: ["interval", "run", "work"] },
+    type: { control: 'radio', option: ['interval', 'run', 'work'] },
   },
-};
+}
 
-export const Default = ({ type, ...args }: any) => {
-  return <Flag {...args} type={type} />;
-};
+export const Default = ({ type = 'interval', ...args }: any) => {
+  return <Flag {...args} type={type} />
+}
