d98fe66ef6f3163bad605b4a58518c534d371fd0
llbllhllk <rkdqudgus9806@gmail.com>
2023-07-24 21:53:25 +0900

    Feat: Create a Slide Component and stories

---
+++ src/stories/UI/Slide.stories.tsx
@@ -0,0 +1,10 @@
+import Slide from "@components/Main/Slide";
+
+export default {
+  title: "Component/Main/Slide",
+  component: Slide,
+};
+
+export const Default = (args: any) => {
+  return <Slide {...args} />;
+};
