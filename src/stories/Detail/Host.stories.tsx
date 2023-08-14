551e4583f11c2241c0cd27484a44669b7f246249
llbllhllk <rkdqudgus9806@gmail.com>
2023-08-14 20:56:55 +0900

    Feat: Create Join component and stories

---
+++ src/stories/Detail/Host.stories.tsx
@@ -0,0 +1,10 @@
+import Host from "@components/Detail/Host";
+
+export default {
+  title: "Component/Detail/Host",
+  component: "Host",
+};
+
+export const Default = (args: any) => {
+  return <Host {...args} />;
+};
---
+++ src/stories/Detail/Join.stories.tsx
@@ -0,0 +1,18 @@
+import GlobalStyle from "@assets/styles/GlobalStyle";
+import GlobalThemeProvider from "@assets/styles/GlobalThemeProvider";
+import Join from "@components/Detail/Join";
+
+export default {
+  title: "Component/Detail/Join",
+  component: Join,
+};
+
+export const Default = (args: any) => {
+  return (
+    <>
+      <GlobalThemeProvider>
+        <Join {...args} />
+      </GlobalThemeProvider>
+    </>
+  );
+};
