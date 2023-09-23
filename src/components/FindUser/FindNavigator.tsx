ceb3987e671b324dd3e37f7e18d6722bb901fc93
kangbyeonghyeon <rkdqudgus9806@gmail.com>
2023-09-23 21:26:02 +0900

    Feat: Create FindUser component

---
+++ src/components/FindUser/FindNavigator.tsx
@@ -0,0 +1,53 @@
+import styled from '@emotion/styled'
+import { useState } from 'react'
+
+interface Props {
+  onTab(tabName: string): void
+}
+
+const FindNavigator = ({ onTab }: Props) => {
+  const [selectedTab, setSelectedTab] = useState<string>('')
+
+  const handleSetTab = (tabName: string) => {
+    setSelectedTab(tabName)
+    onTab(tabName)
+  }
+
+  return (
+    <Container>
+      <Navigator
+        onClick={() => handleSetTab('id')}
+        active={selectedTab === 'id'}
+      >
+        아이디 찾기
+      </Navigator>
+      <Navigator
+        onClick={() => handleSetTab('pw')}
+        active={selectedTab === 'pw'}
+      >
+        비밀번호 찾기
+      </Navigator>
+    </Container>
+  )
+}
+
+export default FindNavigator
+
+const Container = styled.ul`
+  width: 100%;
+  display: flex;
+  align-items: center;
+  height: 22px;
+  padding: 0;
+`
+
+const Navigator = styled.li<{ active: boolean }>`
+  width: 50%;
+  color: #989491;
+  text-align: center;
+  line-height: 22px;
+  font-size: 12px;
+  border-bottom: ${({ active }) => (active ? `1px solid #ff9704` : undefined)};
+  height: 100%;
+  cursor: pointer;
+`
